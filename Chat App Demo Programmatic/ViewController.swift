52288a92e4b3c41aa8ace2db406f331389f7aba2
CeriOSR <CeriOSrey@gmail.com>
2016-12-31 09:43:14 -0800

    Initial Commit

---
+++ Chat App Demo Programmatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat App Demo Programmatic
+//
+//  Created by Rey Cerio on 2016-12-31.
+//  Copyright © 2016 CeriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
